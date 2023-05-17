c96e9d63aaaf0d0ae5d43f5d8884f39d451841a7
Parabeac-Cloud <admin@parabeac.com>
2023-05-17 11:41:19 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/traveller_app_colors.g.dart
@@ -0,0 +1,16 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TravellerAppColors {
+
+static const Color baseGray40 = Color(0xffb5bec6);
+
+static const Color baseGray80 = Color(0xff4a5660);
+
+static const Color baseWhite = Color(0xffffffff);
+}
